4f5632fbad9aad32fd6aded0af2bd43b40ebf220
Zacarías <zacarias@wichipu.com>
2024-03-23 15:48:05 +0100

    feat: Implemented the initial vending machine persistence implementation.

---
+++ src/VendingMachine/Infrastructure/Persistence/JsonFileVendingMachineRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\VendingMachine\Infrastructure\Persistence;
+
+use Acme\VendingMachine\Domain\VendingMachine;
+use Acme\VendingMachine\Domain\VendingMachineRepository;
+use Override;
+
+final readonly class JsonFileVendingMachineRepository implements VendingMachineRepository
+{
+    #[Override]
+    public function save(VendingMachine $vendingMachine): void {}
+}
