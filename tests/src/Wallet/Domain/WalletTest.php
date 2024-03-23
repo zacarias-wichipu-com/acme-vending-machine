59f119e381eb6f9e71e935820ecd76440fcb80dc
Zacarías <zacarias@wichipu.com>
2024-03-23 18:10:58 +0100

    test: Added more wallet exchange amount test cases.

--- tests/src/Wallet/Domain/WalletTest.php
+++ tests/src/Wallet/Domain/WalletTest.php
@@ -9,6 +9,7 @@ use Acme\Coin\Domain\Coin;
 use Acme\Wallet\Domain\CoinBox;
 use Acme\Wallet\Domain\Coins;
 use Acme\Wallet\Domain\Wallet;
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 class WalletTest extends TestCase
@@ -16,19 +17,38 @@ class WalletTest extends TestCase
     /**
      * It Should Calculate Exchange Amount
      *
+     * @dataProvider calculateAmountData
      * @group wallet
      * @group unit
      */
-    public function testItShouldCalculateExchangeAmount(): void
+    public function testItShouldCalculateExchangeAmount(Wallet $wallet, int $amount): void
     {
-        $wallet = Wallet::create(
-            exchangeCoins: Coins::create([
-                CoinBox::create(Coin::createFromAmountInCents(AmountInCents::ONE_HUNDRED), 1),
-                CoinBox::create(Coin::createFromAmountInCents(AmountInCents::TWENTY_FIVE), 1),
-                CoinBox::create(Coin::createFromAmountInCents(AmountInCents::TEN), 1),
-            ]),
-            customerCoins: Coins::create([])
-        );
-        $this->assertEquals(135, $wallet->exchangeAmount());
+        $this->assertEquals(expected: $amount, actual: $wallet->exchangeAmount());
+    }
+
+
+    public static function calculateAmountData(): Generator
+    {
+        yield [
+            Wallet::create(
+                exchangeCoins: Coins::create([
+                    CoinBox::create(Coin::createFromAmountInCents(AmountInCents::ONE_HUNDRED), 1),
+                    CoinBox::create(Coin::createFromAmountInCents(AmountInCents::TWENTY_FIVE), 1),
+                    CoinBox::create(Coin::createFromAmountInCents(AmountInCents::TEN), 1),
+                ]),
+                customerCoins: Coins::create([])
+            ),
+            135,
+        ];
+        yield [
+            Wallet::create(
+                exchangeCoins: Coins::create([
+                    CoinBox::create(Coin::createFromAmountInCents(AmountInCents::FIVE), 1),
+                    CoinBox::create(Coin::createFromAmountInCents(AmountInCents::ONE_HUNDRED), 5),
+                ]),
+                customerCoins: Coins::create([])
+            ),
+            505,
+        ];
     }
 }
