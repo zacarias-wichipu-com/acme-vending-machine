9c45ebd7a160486424d5ae028acca4f30a565826
Zacarías <zacarias@wichipu.com>
2024-03-23 16:02:55 +0100

    test: Tested get a default vending machine from the JSON file vending machine repository.

--- tests/src/VendingMachine/Infrastructure/Persistence/JsonFileVendingMachineRepositoryTest.php
+++ tests/src/VendingMachine/Infrastructure/Persistence/JsonFileVendingMachineRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Acme\VendingMachine\Infrastructure\Persistence;
 
+use Acme\VendingMachine\Domain\VendingMachine;
 use Acme\VendingMachine\Domain\VendingMachineRepository;
 use Acme\VendingMachine\Infrastructure\Persistence\JsonFileVendingMachineRepository;
 use Tests\Acme\Shared\Infrastructure\PhpUnit\AppContextInfrastructureTestCase;
@@ -12,15 +13,32 @@ use Tests\Acme\VendingMachine\Domain\VendingMachineMother;
 class JsonFileVendingMachineRepositoryTest extends AppContextInfrastructureTestCase
 {
     /**
-     * Sabe A Default Vending Machine
+     * It Should Save A Default Vending Machine
      *
      * @group json_file_vending_machine_repository
      * @group integration
      */
-    public function testShouldSaveADefaultVendingMachine(): void
+    public function testItShouldSaveADefaultVendingMachine(): void
     {
         $repository = new JsonFileVendingMachineRepository();
-        $repository->save(VendingMachineMother::defaultMachine());
+        $repository->save(VendingMachine::createDefault());
         $this->assertInstanceOf(VendingMachineRepository::class, $repository);
+    }
+
+    /**
+     * It Should Be Able To Get A Default Vending Machine
+     *
+     * @group json_file_vending_machine_repository
+     */
+    public function testItShouldBeAbleToGetADefaultVendingMachine(): void
+    {
+        $repository = new JsonFileVendingMachineRepository();
+        $repository->save(VendingMachine::createDefault());
+        $defaultVendingMachine = $repository->get();
+        $expectedDefaultVendingMachine = VendingMachineMother::defaultMachine();
+        $this->assertEquals(
+            expected: $expectedDefaultVendingMachine->status(),
+            actual: $defaultVendingMachine->status()
+        );
     }
 }
