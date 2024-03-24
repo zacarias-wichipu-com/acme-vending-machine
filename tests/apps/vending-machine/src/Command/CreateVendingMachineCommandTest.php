097b55a6f900956223af8d0203fb717d7f4d425a
Zacarías <zacarias@wichipu.com>
2024-03-24 11:37:05 +0100

    test: Tested vending machine create command.

---
+++ tests/apps/vending-machine/src/Command/CreateVendingMachineCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Acme\Ui\Cli\Command;
+
+use Acme\Ui\Cli\Command\CreateVendingMachineCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\Acme\Shared\Infrastructure\PhpUnit\AppContextInfrastructureTestCase;
+
+class CreateVendingMachineCommandTest extends AppContextInfrastructureTestCase
+{
+    /**
+     * It Should Return A Success Response
+     *
+     * @group create_vending_machine_command
+     * @group application
+     */
+    public function testItShouldReturnASuccessResponse(): void
+    {
+        $command =  new CreateVendingMachineCommand();
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+        $commandTester->assertCommandIsSuccessful();
+    }
+}
