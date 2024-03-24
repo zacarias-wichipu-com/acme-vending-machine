f744337a99e0f08d1b678507887ecc824f8dcdf0
Zacarías <zacarias@wichipu.com>
2024-03-24 13:22:51 +0100

    test: Added print machine command test.

--- tests/apps/vending-machine/src/Command/PrintVendingMachineCommandTest.php
+++ tests/apps/vending-machine/src/Command/PrintVendingMachineCommandTest.php
@@ -4,21 +4,20 @@ declare(strict_types=1);
 
 namespace Tests\Acme\Ui\Cli\Command;
 
-use Acme\Ui\Cli\Command\InitVendingMachineCommand;
 use Symfony\Component\Console\Tester\CommandTester;
 use Tests\Acme\Shared\Infrastructure\PhpUnit\AppContextInfrastructureTestCase;
 
-class InitVendingMachineCommandTest extends AppContextInfrastructureTestCase
+class PrintVendingMachineCommandTest extends AppContextInfrastructureTestCase
 {
     /**
      * It Should Return A Success Response
      *
-     * @group init_vending_machine_command
+     * @group print_vending_machine_command
      * @group application
      */
     public function testItShouldReturnASuccessResponse(): void
     {
-        $command =  new InitVendingMachineCommand($this->service('console_command.messenger_bus'));
+        $command =  new PrintVendingMachineCommand($this->service('console_command.messenger_bus'));
         $commandTester = new CommandTester($command);
         $commandTester->execute([]);
         $commandTester->assertCommandIsSuccessful();
